1078e5397ec5e09359ad1cf105434db8cc5b49e6
Endinity <164420248+Li-15@users.noreply.github.com>
2024-12-30 14:03:34 -0600

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display")
+
+function appendToDisplay(value) {
+    display.value += value
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    }
+    catch (error) {
+        display.value = "error"
+    }
+}
+
+function clear() {
+    display.value = ""
+}
